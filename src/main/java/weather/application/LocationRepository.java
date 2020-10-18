f95f36e11d10d892fbe4ce357abda02fd92aefaf
Michal Paukszto <michal.paukszto@gmail.com>
2020-10-18 12:30:04 +0200

    refactor

--- src/main/java/weather/application/LocationRepository.java
+++ src/main/java/weather/application/LocationRepository.java
@@ -1,16 +1,25 @@
 package weather.application;
 
+import org.hibernate.SessionFactory;
+
+import java.util.Collections;
 import java.util.List;
 
-public class LocationRepository {   //warstwa danych
+public class LocationRepository {   // warstwa danych
+
+    private final SessionFactory sessionFactory;
+
+    public LocationRepository() {
+        // todo: add the hibernate configuration
+
+        sessionFactory = null;
+    }
 
     public Location saveNewLocation(Location location) {
-
-        return null;
+        return location;
     }
 
     public List<Location> readAllLocationsSaved() {
-
-        return null;
+        return Collections.emptyList();
     }
 }
---
+++ src/main/test/java/weather/application/LocationServiceTest.java
@@ -0,0 +1,49 @@
+package weather.application;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class LocationServiceTest {
+
+    private final LocationService locationService = new LocationService();
+
+    @Test
+    void addNewLocation_shouldReturnNewLocation() {
+        // when
+        Location location = locationService.addNewLocation("city", 0, 0, "region", "country");
+
+        // then
+        assertEquals(location.getCityName(), "city");
+        assertEquals(location.getCountryName(), "country");
+        assertEquals(location.getRegionName(), "region");
+        assertEquals(location.getLongitude(), 0);
+        assertEquals(location.getLatitude(), 0);
+    }
+
+    @Test
+    void addNewLocation_whenRegionIsEmpty_shouldReturnNewLocation() {
+        // when
+        Location location = locationService.addNewLocation("city", 0, 0, "", "country");
+
+        // then
+        assertEquals(location.getCityName(), "city");
+        assertEquals(location.getCountryName(), "country");
+        assertEquals(location.getRegionName(), "");
+        assertEquals(location.getLongitude(), 0);
+        assertEquals(location.getLatitude(), 0);
+    }
+
+    @Test
+    void addNewLocation_whenCityIsEmpty_shouldThrowException() {
+        // when
+        assertThrows(Exception.class, () -> locationService.addNewLocation(" ", 0, 0, "region", "country"));
+    }
+
+    @Test
+    void addNewLocation_whenLongitudeIsAbove180_shouldThrowException() {
+        // when
+        assertThrows(Exception.class, () -> locationService.addNewLocation("city", 0, 181, "region", "country"));
+    }
+}
