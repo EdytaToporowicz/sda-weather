56a4ccc6111aacb33ac006c26e6bf2282f84bb55
Michal Paukszto <michal.paukszto@gmail.com>
2020-10-18 10:43:43 +0200

    refactor

--- src/main/java/weather/application/LocationController.java
+++ src/main/java/weather/application/LocationController.java
@@ -1,19 +1,27 @@
 package weather.application;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.util.List;
 
 public class LocationController {   //warstwa prezentacji
-    LocationService locationService = new LocationService();
+
+    private final LocationService locationService = new LocationService();
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     public String addLocation(String cityName, int latitude, int longitude, String regionName, String countryName) {
         Location location = locationService.addNewLocation(cityName, latitude, longitude, regionName, countryName);
-        return location.toString();
+
+        try {
+            return objectMapper.writeValueAsString(location);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("...");
+        }
     }
 
     public  String readAllLocations() {
         List<Location> locations = locationService.readAllLocationsSaved();
         return "";
     }
-
-
 }
--- src/main/java/weather/application/LocationService.java
+++ src/main/java/weather/application/LocationService.java
@@ -4,15 +4,18 @@ import java.util.List;
 
 public class LocationService {//warstwa logiki biznesowej
 
-    LocationRepository locationRepository=new LocationRepository();
+    LocationRepository locationRepository = new LocationRepository();
 
 
     public Location addNewLocation(String cityName, int latitude, int longitude, String regionName, String countryName) {
         //in progress......
 
+        return null;
     }
 
     public List<Location> readAllLocationsSaved() {
         //in progress......
+
+        return null;
     }
 }
