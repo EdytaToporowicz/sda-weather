159174a833d0013fa7988e1a2e9ec8dd4e9df452
Michal Paukszto <michal.paukszto@gmail.com>
2020-10-18 12:55:53 +0200

    refactor

--- src/main/java/weather/application/Location.java
+++ src/main/java/weather/application/Location.java
@@ -1,7 +1,8 @@
 package weather.application;
 
+import lombok.Data;
 
-
+@Data
 public class Location {
     private String cityName;
     private double latitude;       //szer.geog
@@ -14,46 +15,6 @@ public class Location {
         this.latitude = latitude;
         this.longitude = longitude;
         this.regionName = regionName;
-        this.countryName = countryName;
-    }
-
-    public String getCityName() {
-        return cityName;
-    }
-
-    public double getLatitude() {
-        return latitude;
-    }
-
-    public double getLongitude() {
-        return longitude;
-    }
-
-    public String getRegionName() {
-        return regionName;
-    }
-
-    public String getCountryName() {
-        return countryName;
-    }
-
-    public void setCityName(String cityName) {
-        this.cityName = cityName;
-    }
-
-    public void setLatitude(double latitude) {
-        this.latitude = latitude;
-    }
-
-    public void setLongitude(double longitude) {
-        this.longitude = longitude;
-    }
-
-    public void setRegionName(String regionName) {
-        this.regionName = regionName;
-    }
-
-    public void setCountryName(String countryName) {
         this.countryName = countryName;
     }
 }
--- src/test/java/weather/application/LocationServiceTest.java
+++ src/test/java/weather/application/LocationServiceTest.java
@@ -1,4 +1,5 @@
 package weather.application;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
